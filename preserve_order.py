372c4f6a4933dcec07d0e1b1b861e28aefbc318a
jjohnson524 <107004088+jjohnson524@users.noreply.github.com>
2025-05-08 12:13:41 -0500

    Update preserve_order.py

--- preserve_order.py
+++ preserve_order.py
@@ -493,7 +493,7 @@ def preserve_order_obstruct(aut,k,kact=-1,kconj=-1,kprod=-1,order=1,track_extra_
         kconj=k
     if kprod==-1:
         kprod=k
-    F = Automorphism.FreeGroup1(aut.rank)
+    F = Automorphism.FreeGroup1(aut.rank())
 #    if Print:        
 #        print('Input Braid: '+str(gens))
 #        f=braid.action(optimize=True)
